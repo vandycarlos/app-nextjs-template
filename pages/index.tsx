be880c8ade45d1a496497f29b58384c88882ddfe
vandycarlos <vandycarlos@gmail.com>
2019-09-20 13:29:52 -0400

    x

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,17 +3,16 @@ import Link from 'next/link'
 import Layout from '../components/Layout'
 import { NextPage } from 'next'
 
-const Home: NextPage = () => {
-  return (
-    <Layout title="Home | App">
-      <h1>Hello App 👋</h1>
-      <p>
-        <Link href="/about">
-          <a>About</a>
-        </Link>
-      </p>
-    </Layout>
-  )
-}
+const Home: NextPage = () => (
+  <Layout title="Home | App">
+    <h1>Hello App 👋</h1>
+    <p>
+      <Link href="/about">
+        <a>About</a>
+      </Link>
+    </p>
+  </Layout>
+)
+
 
 export default Home
